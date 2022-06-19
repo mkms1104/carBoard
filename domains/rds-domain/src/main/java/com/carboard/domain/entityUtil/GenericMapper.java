1ce27d4b457bdc7661af4c4c293e99ac4d6cb21c
mskim <mskim@11h11m.com>
2022-06-19 14:40:42 +0900

    mapStruct 테스트 코드 추가

--- domains/rds-domain/src/main/java/com/carboard/domain/entityUtil/GenericMapper.java
+++ domains/rds-domain/src/main/java/com/carboard/domain/entityUtil/GenericMapper.java
@@ -1,13 +1,6 @@
 package com.carboard.domain.entityUtil;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
 public interface GenericMapper<D, E> {
     D toDto(E e);
     E toEntity(D d);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void updateFromDto(D dto, @MappingTarget E entity);
 }
