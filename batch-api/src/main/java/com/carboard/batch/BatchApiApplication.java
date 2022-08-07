9185968bfce7237f823f0ba44f87f946ed6a40e7
mskim <mskim@11h11m.com>
2022-08-07 16:06:06 +0900

    모듈 소스 파일 정리, gitignore 루트 프로젝트에서 공통 처리

---
+++ batch-api/src/main/java/com/carboard/batch/BatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.carboard.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApiApplication.class, args);
+    }
+
+}
