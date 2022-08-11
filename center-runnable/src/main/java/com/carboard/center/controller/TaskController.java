09d988cc2daec360e702bc0017b66c5c42f98992
tnsska2 <vnemftnsska2@naver.com>
2022-08-11 23:34:14 +0900

    feat: getTaskList 임시 구현
    TODO: TaskController

--- center-runnable/src/main/java/com/carboard/center/controller/TaskController.java
+++ center-runnable/src/main/java/com/carboard/center/controller/TaskController.java
@@ -1,15 +1,22 @@
 package com.carboard.center.controller;
 
+import com.carboard.domain.task.Task;
 import com.carboard.domain.task.TaskDto;
+import com.carboard.domain.task.TaskMapper;
 import com.carboard.domain.task.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("task")
+@RequestMapping("/v1/task")
 public class TaskController {
     private final TaskRepository taskRepository;
 
@@ -17,5 +24,20 @@ public class TaskController {
     public String testRoute(TaskDto taskDto) {
         System.out.println(taskDto);
         return "test";
+    }
+
+    @GetMapping
+    public List<TaskDto> getTaskList() {
+        List<Task> tasks = taskRepository.findAll();
+        return tasks.stream()
+            .map(v -> TaskMapper.INSTANCE.toDto(v))
+            .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String createTask(TaskDto taskForm) {
+
+
+        return "";
     }
 }
